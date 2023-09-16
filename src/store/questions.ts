feac2c50a918b732c0adf2664f4e4867bd9bd9a1
jeresc <jeremiastomassrc@gmail.com>
2023-09-16 14:36:39 -0300

    refactor: ♻ moving fetch questions from store to services and add store config to work w/ redux devtools

--- src/store/questions.ts
+++ src/store/questions.ts
@@ -1,7 +1,8 @@
 import { create } from 'zustand'
 import { Question } from '@/types/types'
 import confetti from 'canvas-confetti'
-import { persist } from 'zustand/middleware'
+import { persist, devtools } from 'zustand/middleware'
+import { getAllQuestions } from '@/services/questions'
 
 interface QuestionsState {
   questions: Question[]
@@ -13,66 +14,90 @@ interface QuestionsState {
   reset: () => void
 }
 
+{
+  /* 
+   // Logger middleware example
+const logger = (config) => (set, get, api) =>
+  config(
+    (...args) => {
+      console.log('applying', args)
+      set(...args)
+      console.log('applied', args)
+    },
+    get,
+    api
+  )
+*/
+}
+
 export const useQuestionsStore = create<QuestionsState>()(
-  persist(
-    (set, get) => ({
-      questions: [],
-      currentQuestionIndex: 0,
+  devtools(
+    persist(
+      (set, get) => ({
+        questions: [],
+        currentQuestionIndex: 0,
 
-      fetchQuestions: async (limit: number) => {
-        const res = await fetch('http://localhost:5173/data.json')
-        const json = await res.json()
+        fetchQuestions: async (limit: number) => {
+          const data = await getAllQuestions()
+          const questions = data.sort(() => Math.random() - 0.5).slice(0, limit)
+          set({ questions }, false, 'FETCH_QUESTIONS')
+        },
 
-        const questions = json.sort(() => Math.random() - 0.5).slice(0, limit)
-        set({ questions })
-      },
+        selectAnswer: (questionId: number, answerIndex: number) => {
+          const { questions } = get()
+          const newQuestions = structuredClone(questions)
 
-      selectAnswer: (questionId: number, answerIndex: number) => {
-        const { questions } = get()
-        const newQuestions = structuredClone(questions)
+          const questionIndex = newQuestions.findIndex(
+            (question) => question.id === questionId
+          )
+          const questionInfo = newQuestions[questionIndex]
 
-        const questionIndex = newQuestions.findIndex(
-          (question) => question.id === questionId
-        )
-        const questionInfo = newQuestions[questionIndex]
+          const isUserAnswerCorrect = questionInfo.correctAnswer === answerIndex
 
-        const isUserAnswerCorrect = questionInfo.correctAnswer === answerIndex
+          if (isUserAnswerCorrect) confetti()
 
-        if (isUserAnswerCorrect) confetti()
+          newQuestions[questionIndex] = {
+            ...questionInfo,
+            isUserAnswerCorrect,
+            userSelectedAnswer: answerIndex,
+          }
 
-        newQuestions[questionIndex] = {
-          ...questionInfo,
-          isUserAnswerCorrect,
-          userSelectedAnswer: answerIndex,
-        }
+          set({ questions: newQuestions }, false, 'SELECT_ANSWER')
+        },
 
-        set({ questions: newQuestions })
-      },
+        goNextQuestion: () => {
+          const { currentQuestionIndex, questions } = get()
+          const nextQuestionIndex = currentQuestionIndex + 1
 
-      goNextQuestion: () => {
-        const { currentQuestionIndex, questions } = get()
-        const nextQuestionIndex = currentQuestionIndex + 1
+          if (nextQuestionIndex < questions.length) {
+            set(
+              { currentQuestionIndex: nextQuestionIndex },
+              false,
+              'GO_NEXT_QUESTION'
+            )
+          }
+        },
 
-        if (nextQuestionIndex < questions.length) {
-          set({ currentQuestionIndex: nextQuestionIndex })
-        }
-      },
+        goPreviousQuestion: () => {
+          const { currentQuestionIndex } = get()
+          const prevQuestionIndex = currentQuestionIndex - 1
 
-      goPreviousQuestion: () => {
-        const { currentQuestionIndex } = get()
-        const prevQuestionIndex = currentQuestionIndex - 1
+          if (prevQuestionIndex >= 0) {
+            set(
+              { currentQuestionIndex: prevQuestionIndex },
+              false,
+              'GO_PREVIOUS_QUESTION'
+            )
+          }
+        },
 
-        if (prevQuestionIndex >= 0) {
-          set({ currentQuestionIndex: prevQuestionIndex })
-        }
-      },
-
-      reset: () => {
-        set({ questions: [], currentQuestionIndex: 0 })
-      },
-    }),
-    {
-      name: 'questions',
-    }
+        reset: () => {
+          set({ questions: [], currentQuestionIndex: 0 }, false, 'RESET')
+        },
+      }),
+      {
+        name: 'questions',
+      }
+    )
   )
 )
